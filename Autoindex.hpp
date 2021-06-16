03a47556af7c787ca21be5095b1266e5fc7c09ab
kbatwoma <kbatwoma@student.21-school.ru>
2021-06-16 12:56:55 +0300

    auto

---
+++ Autoindex.hpp
@@ -0,0 +1,16 @@
+#ifndef AUTIONDEX_HPP
+# define AUTIONDEX_HPP
+
+class Autoindex
+{
+    public:
+        Autoindex();
+        Autoindex(Autoindex const &);
+        Autoindex &operator=(Autoindex const &);
+        ~Autoindex();
+
+    private:
+
+};
+
+#endif
