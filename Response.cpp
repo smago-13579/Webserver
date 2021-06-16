a361222b806d2a452e525ddb73b5d38a31102ae1
smago <smago@student.21-school.ru>
2021-06-16 23:53:32 +0300

    Handle errors

--- Response.cpp
+++ Response.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Response.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smago <smago@student.42.fr>                +#+  +:+       +#+        */
+/*   By: smago <smago@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/11 17:31:33 by smago             #+#    #+#             */
-/*   Updated: 2021/06/15 19:12:31 by smago            ###   ########.fr       */
+/*   Updated: 2021/06/16 23:29:54 by smago            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ Response::Response(const Request& req, Settings set)
 	this->req = req;
 	this->response_done	= 0;
 	this->settings = &set;
-	content_type = this->req.headers["Accept"];
 	find_method();
 }
 
@@ -34,6 +33,64 @@ Response&	Response::operator=(const Response& tmp)
 	response_done = tmp.response_done;
 
 	return (*this);
+}
+
+/******************/
+/*                */
+/*   Error pages  */
+/*                */
+/******************/
+int			Response::error_page(int i)
+{
+	std::stringstream	response, body, file;
+	int fd, res;
+	char buff[100000];
+
+	file << settings->error_page << "/" << "error_" \
+	<< itoa(i) << ".html";
+	std::cout << "FILE: " << file.str() << std::endl;
+
+	if ((fd = open(file.str().c_str(), O_RDONLY)) < 0) {
+		std::cout << "CAN'T OPEN FILE: " << file.str() << std::endl;
+		return (-1);
+	}
+	while ((res = read(fd, buff, 99999)) > 0) {
+		buff[res] = '\0';
+		body << buff;
+	}
+	body << "\r\n\r\n";
+	close(fd);
+	if (res < 0) {
+		std::cout << "CAN'T READ FILE: " << file.str() << std::endl;
+		return (-1);
+	}
+
+	response << req.version << " " << itoa(i) << status_codes(i)
+	<< "Version: " << req.version << "\r\n"
+	<< get_headers(file.str());
+	response << "Content-Length: " << body.str().length()
+	<< "\r\n\r\n" << body.str();
+	this->answer = response.str();
+	
+	this->response_done	= 1;
+	
+	return (0);
+}
+
+std::string			Response::status_codes(int i)
+{
+	std::string str;
+
+	if (i == 400)
+		str = " Bad Request\r\n";
+	else if (i == 403)
+		str = " Forbidden\r\n";
+	else if (i == 404)
+		str = " Not Found\r\n";
+	else if (i == 500)
+		str = " Internal Server Error\r\n";
+
+	return (str);
 }
 
 int			Response::compare_prefix(std::string loc, std::string res)
@@ -52,8 +109,6 @@ int			Response::compare_prefix(std::string loc, std::string res)
 
 int			Response::check_method(std::vector<size_t>& methods, size_t cmd)
 {
-	if (methods.size() == 0)
-		return (1);
 	for (std::vector<size_t>::iterator it = methods.begin(); it != methods.end(); it++)
 	{
 		if (cmd == *it)
@@ -70,45 +125,50 @@ int			Response::create_response(const Location& loc)
 	int fd, res;
 	char buff[100000];
 
-	if (req.resource == loc.location)
-		file << loc.root << "/" << loc.index;
-	else 
-		file << loc.root << req.resource;
+	file << get_path(loc);
 	std::cout << "FILE: " << file.str() << std::endl;
 	
-	if (file.str().find(".png") == std::string::npos && \
-		file.str().find(".PNG") == std::string::npos && \
-		file.str().find(".ico") == std::string::npos && \
-		file.str().find(".jpeg") == std::string::npos && 
-		file.str().find(".py") == std::string::npos)
+	if (get_format(file.str()) == TEXT)
 	{
 		if ((fd = open(file.str().c_str(), O_RDONLY)) < 0) {
 			std::cout << "CAN'T OPEN FILE: " << file.str() << std::endl;
+			if (errno == EACCES)
+				error_page(403);
+			else if (errno == ENOENT)
+				error_page(404);
 			return (-1);
 		}
-		res = read(fd, buff, 999999);
-		buff[res] = '\0';
+		while ((res = read(fd, buff, 99999)) > 0) {
+			buff[res] = '\0';
+			body << buff;
+		}
+		body << "\r\n\r\n";
+		close(fd);
+		if (res < 0) {
+			std::cout << "CAN'T READ FILE\n";
+			return (-1);
+		}
 	}
 	else
+	{
 		image.open(file.str(), std::ifstream::binary);
-
-	if (image.is_open())
+		if (!image.is_open()) {
+			std::cout << "CAN'T OPEN FILE: " << file.str() << std::endl;
+			return (-1);
+		}
 		body << image.rdbuf() << "\r\n\r\n";
-	else {
-		body << buff << "\r\n\r\n";
+		image.clear();
+		image.close();
 	}
+
 	response << req.version << " 200 OK\r\n"
 	<< "Version: " << req.version << "\r\n"
-	<< get_headers();
+	<< get_headers(file.str());
 	response << "Content-Length: " << body.str().length()
 	<< "\r\n\r\n" << body.str();
 	this->answer = response.str();
 	
 	this->response_done	= 1;
-
-	body.clear();
-	image.clear();
-	image.close();
 	
 	return (0);
 }
@@ -117,7 +177,7 @@ Response::loc_iter	Response::find_location()
 {
 	int i = 0;
 	int comp;
-	loc_iter itn;
+	loc_iter itn = settings->locations.begin();
 	
 	for (loc_iter it = settings->locations.begin(); it != settings->locations.end(); it++)
 	{
@@ -170,20 +230,13 @@ void		Response::find_method()
 		for (; begin != req.headers.end(); begin++) {
 			vec.push_back("HTTP_" + begin->first + "=" + begin->second);
 		}
-		for (std::vector<std::string>::iterator begin = vec.begin(); begin != vec.end(); begin++)
-		{
-			std::cout << *begin << std::endl;
-		}
+		// for (std::vector<std::string>::iterator begin = vec.begin(); begin != vec.end(); begin++)
+		// {
+		// 	std::cout << *begin << std::endl;
+		// }
 		
 	// }
 	/*  		ADD ANOTHER METHODS				*/
-}
-
-std::string			Response::get_response() 
-{
-	if (response_done)
-		return (this->answer); 
-	return ("");
 }
 
 void				Response::erase_answer(int i)
@@ -191,23 +244,55 @@ void				Response::erase_answer(int i)
 	this->answer.erase(0, i);
 }
 
-std::string			Response::get_headers()
+int					Response::get_format(std::string str)
+{
+	if (str.find(".png") == std::string::npos && \
+	str.find(".PNG") == std::string::npos && \
+	str.find(".ico") == std::string::npos && \
+	str.find(".jpeg") == std::string::npos && \
+	str.find(".jpg") == std::string::npos && 
+	str.find(".py") == std::string::npos)
+		return (TEXT);
+	return (IMAGE);
+}
+
+std::string			Response::get_headers(std::string str)
 {
 	std::stringstream headers;
 	time_t raw;
 	time(&raw);
 	
-	content_type = req.headers["Accept"];
-	
-	// if (content_type.compare("text/html") == 0) 
-	content_type = content_type.substr(0, content_type.find(","));
-	content_type += ";";
+	int i = str.rfind(".") + 1;
+	content_type = str.substr(i, str.size() - i);
+	std::cout << content_type << std::endl;
+	if (content_type == "html" || content_type == "css")
+		content_type = "text/" + content_type;
+	else
+		content_type = "image/" + content_type;
 
 	headers << "Server: DreamTeam/1.0.1 (School 21)\r\n"
 	<< "Date: " << ctime(&raw)
 	<< "Content-Type: " << content_type << "\r\n";
 
 	return (headers.str());
+}
+
+std::string			Response::get_path(const Location& loc)
+{
+	std::stringstream str;
+
+	if (req.resource == loc.location)
+		str << loc.root << "/" << loc.index;
+	else
+		str << loc.root << req.resource;
+	return str.str();
+}
+
+std::string			Response::get_response() 
+{
+	if (response_done)
+		return (this->answer);
+	return ("");
 }
 
 int			Response::method_GET()
@@ -221,9 +306,11 @@ int			Response::method_GET()
 		create_response(*it);
 		return (0);
 	}
-	else {
-		std::cout << "\nCan't use get method\n";
-		return (-1);
+	else 
+	{
+		std::cout << "\nBAD REQUEST\n"; // Bad request
+		if (error_page(400) == 0)
+			return (0);
 	}
 	return (-1);
 }
