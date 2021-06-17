7d36c704e713238cd15ea7a71c569646a1b34d0d
CiberAdventureCat <kbatwoma@student.21-school.ru>
2021-06-17 13:48:31 +0300

    autoindex

--- Autoindex.hpp
+++ Autoindex.hpp
@@ -1,7 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Autoindex.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/17 11:05:03 by kbatwoma          #+#    #+#             */
+/*   Updated: 2021/06/17 13:04:27 by kbatwoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef AUTIONDEX_HPP
 # define AUTIONDEX_HPP
 
 # include <iostream>
+# include <vector>
+# include <dirent.h>
 
 class Autoindex
 {
@@ -15,8 +29,10 @@ class Autoindex
 
     private:
         Autoindex();
+        void    _create_body();
 
-        std::string _body;
+        std::string                 _body;
+        std::vector<std::string>    _folder_contents;
 };
 
 #endif
